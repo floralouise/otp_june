70ab1b6e684b1bf8aafbf135b941474c46c8f2dd
Stephanie Wood <wood.steph@gmail.com>
2023-06-22 11:32:58 -0700

    set up game status

--- numble/lib/numble/game.ex
+++ numble/lib/numble/game.ex
@@ -8,11 +8,9 @@ defmodule Numble.Game do
 
   def make_guess(game, guess) do
     guesses = [guess | game.guesses]
-    %{game | guesses: guesses}
-  end
 
-  def show_rows(answer, guesses) do
-    Enum.map(guesses, fn guess -> show_row(answer, guess) end)
+    %{game | guesses: guesses}
+    |> apply_status()
   end
 
   def show(game) do
@@ -20,25 +18,44 @@ defmodule Numble.Game do
     ? ? ? ?
     #{show_rows(game.answer, game.guesses)}
 
-    status: playing
+    status: #{game.status}
     """
   end
 
-  def show_row(answer, guess) do
+  defp show_rows(answer, guesses) do
+    Enum.map(guesses, fn guess -> show_row(answer, guess) end)
+  end
+
+  defp show_row(answer, guess) do
     "#{Enum.join(guess, " ")} | #{show_score(answer, guess)}\n"
   end
 
-  def show_score(answer, guess) do
+  defp show_score(answer, guess) do
     reds =
       Enum.zip(answer, guess)
       |> Enum.filter(fn {a, b,} -> a == b end)
       |> length()
-    missing =
+      missing =
       (guess -- answer) |> length
-    whites =
+      whites =
       (4 - reds) - missing
 
     String.duplicate("R", reds) <> String.duplicate("W", whites)
+  end
+
+  defp apply_status(game) do
+    %{game | status: status(game)}
+  end
+
+  defp status(%{ answer: answer, guesses: [answer | _guesses]}) do
+    :won
+  end
+
+  defp status(game) do
+    cond do
+      length(game.guesses) < 10 -> :playing
+      true -> :lost
+    end
   end
 
 end
