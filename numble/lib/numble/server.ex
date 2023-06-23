399b7b3ea4e0dd1b26ce8bba71f663ec649c7969
Savannah <sm05908@gmail.com>
2023-06-23 10:24:12 -0400

    superheroes

--- numble/lib/numble/server.ex
+++ numble/lib/numble/server.ex
@@ -2,28 +2,34 @@ defmodule Numble.Server do
   use GenServer
   alias Numble.Game
 
-  def start_link() do
-    GenServer.start_link(__MODULE__, Game.new())
+  def start_link(name \\ :numble) do
+    IO.puts("starting #{name}")
+    GenServer.start_link(__MODULE__, Game.new(), name: name)
   end
 
   def init(game) do
     {:ok, game}
   end
 
-  def handle_cast({:guess, guess}, game) do
+  def handle_call({:guess, guess}, _from, game) do
     new_game = Game.make_guess(game, guess)
-    {:noreply, new_game}
+    {:reply, Game.show(new_game), new_game}
   end
 
-  def handle_call(:show, _from, game) do
-    {:reply, Game.show(game), game}
+  # def handle_call(:show, _from, game) do
+  #   {:reply, Game.show(game), game}
+  # end
+
+  def guess(pid \\ :numble, guess) do
+    GenServer.call(pid, {:guess, guess})
+    |> IO.puts
   end
 
-  def guess(pid, guess) do
-    GenServer.cast(pid, {:guess, guess})
-  end
+  # def show(pid) do
+  #   GenServer.call(pid, :show)
+  # end
 
-  def show(pid) do
-    GenServer.call(pid, :show)
+  def child_spec(name) do
+    %{id: [name], start: {Numble.Server, :start_link, [name]}}
   end
 end
